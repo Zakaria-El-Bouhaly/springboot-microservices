e99864c9f9c380c8d3b2e054ff4166a34544bb8f
zakaria <you@example.com>
2023-11-08 02:30:22 +0100

    auth

--- theproject/src/main/resources/data.sql
+++ theproject/src/main/resources/data.sql
@@ -3,8 +3,8 @@ values (1, 'admin'),
        (2, 'user');
 
 insert into users (id, email,is_activated, full_name, password)
-values (1, 'ccasini0@hibu.com', 'Carter Casini',true, '$2a$10$OBKOGCzJu8YmLGxWYJfNlu4RlRkakWnWVNd/ZYw9BvCdbRQVFqdSW')
-     , (2, 'jarmiger1@seesaa.net', 'Josey Armiger',false, '$2a$10$b7/jEOwAvfc6.wU8vvh7.eKbtK4DtvM6yddZk97VAYJ59V4SgO0du');
+values (1, 'ccasini0@hibu.com',true, 'Carter Casini', '$2a$10$OBKOGCzJu8YmLGxWYJfNlu4RlRkakWnWVNd/ZYw9BvCdbRQVFqdSW')
+     , (2, 'jarmiger1@seesaa.net',false,'Josey Armiger', '$2a$10$b7/jEOwAvfc6.wU8vvh7.eKbtK4DtvM6yddZk97VAYJ59V4SgO0du');
 
 
 insert into user_roles (user_id, role_id)
@@ -21,6 +21,6 @@ values (1, 'Bluestem', 'NA', '459-510-5248', 'Itsukaichi', 'http://dummyimage.co
         165.2386673, 2);
 
 insert into bookings (id, check_in, check_out, offer_id, user_id)
-values (1, '1588550400000', '1588550400000', 1, 1),
-       (2, '1588550400000', '1584550400000', 3, 2),
-       (3, '1588550400000', '1588250400000', 4, 1);
+values (1, '2023-05-04', '2023-05-05', 1, 2),
+         (2, '2023-05-04', '2023-05-05', 3, 1),
+         (3, '2023-05-04', '2023-05-05', 4, 2);
